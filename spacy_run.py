b5df27622f449dbc7129b2fa47cc664874a61782
Tom Sherborne <tr.sherborne@gmail.com>
2017-12-27 22:41:26 +0000

    spacy parsing skeleton

---
+++ spacy_run.py
@@ -0,0 +1,19 @@
+import spacy
+import sys
+from collections import namedtuple
+
+if __name__ == '__main__':
+
+	IndexedWord = namedtuple('IndexedWord','word index')
+	DependencyArc = namedtuple('DependencyArc','start_word end_word label')
+	
+	nlp = spacy.load('en')
+	sent = u'Letters delivered on time by old-fashioned means are increasingly rare, so it is as well that that is not the only option available.'
+	doc = nlp(sent)
+	arcs = set()
+
+	for token in doc:
+		newArc = DependencyArc(IndexedWord(token.head.text,token.head.i+1),IndexedWord(token.text,token.i+1),token.dep_)
+		arcs.add(newArc)
+		print(newArc)
+		print()
