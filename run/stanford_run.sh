8fcb72dd45ca6dd61a3de6aa25bbfd327341ef31
Tom Sherborne <tr.sherborne@gmail.com>
2018-01-02 13:31:04 +0000

    standardise parser calls and add dep parsing with punc

--- run/stanford_run.sh
+++ run/stanford_run.sh
@@ -6,10 +6,10 @@ if [ "`basename $(pwd)`" == "parsers" ];
 fi
 
 cp_loc="$(pwd)/src-parsers/stanford-parser-full-2017-06-09"
-input_file_loc="$(pwd)/input/input_raw.txt"
+input_file_loc="$(pwd)/input/selected_sent_raw.txt"
 output_file_loc_pcfg="$(pwd)/output/pcfg_stanford_parse_output.txt"
 output_file_loc_nn="$(pwd)/output/nn_stanford_parse_output.txt"
 
-$cp_loc/lexparser.sh $input_file_loc >> $output_file_loc_pcfg
+java -mx200m -cp "$cp_loc/*" edu.stanford.nlp.parser.lexparser.LexicalizedParser  -model edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz -outputFormat "typedDependencies" -outputFormatOptions includePunctuationDependencies $input_file_loc > $output_file_loc_pcfg
 
-java -Xmx2g -cp "$cp_loc/*" edu.stanford.nlp.parser.nndep.DependencyParser -model edu/stanford/nlp/models/parser/nndep/english_UD.gz -textFile $input_file_loc -outFile $output_file_loc_nn
+java -Xmx2g -cp "$cp_loc/*" edu.stanford.nlp.parser.nndep.DependencyParser -model edu/stanford/nlp/models/parser/nndep/english_UD.gz -outputFormatOptions includePunctuationDependencies -textFile $input_file_loc -outFile $output_file_loc_nn
