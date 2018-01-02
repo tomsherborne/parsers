0297a4b6348f591a7f5fcd57fe48fdca3604edc7
Tom Sherborne <tr.sherborne@gmail.com>
2018-01-02 19:41:03 +0000

    working tex generator

--- viz/dep_generator_test.py
+++ viz/dep_generator_test.py
@@ -6,17 +6,13 @@ from collections import namedtuple
 
 IndexedWord = namedtuple('IndexedWord','word index')
 DependencyArc = namedtuple('DependencyArc','start_word end_word label is_root')
-stanford_re = r'(\(|\)|,| )'
+stanford_re = r'(\(|\)|,)'
 filter_out = ['','(',')','.',',',' ']
 
 doc_preamble = r'''
 \documentclass{article}
 \usepackage[margin=1cm]{geometry}
-\usepackage{tikz}
-\usepackage{pgfplots}
 \usepackage{tikz-dependency}
-\usetikzlibrary{positioning}
-\pgfplotsset{compat=1.14}
 \begin{document}
 '''
 
@@ -33,6 +29,27 @@ fig_postamble = r'''
 '''
 
 
+def _generate_arcs(elems):
+	new_arc = []
+	
+	label = elems[0]
+	is_root = True if elems[0]=='root' else False
+
+	startw = IndexedWord(elems[1],elems[2])
+	endw = IndexedWord(elems[3],elems[4])
+
+	return DependencyArc(startw,endw,label,is_root)
+
+	# for word in elems[1:]:
+	# 	#word_elems = word.replace('_','-').split('-')
+	# 	word_elems = [w for w in word_elems if w is not '' or not ' ']
+	# 	#print(word_elems)
+	# 	new_word = IndexedWord(word_elems[0],int(word_elems[1]))
+	# 	new_arc.append(new_word)
+	# 	new_words.add(new_word)
+	# is_root = True if elems[0]=='root' else False
+	#return DependencyArc(new_arc[0],new_arc[1],elems[0],is_root),new_words
+
 def generate_dep_arcs_stanford(input_string):
 	'''
 	Take list of grammatical relations and return
@@ -40,27 +57,77 @@ def generate_dep_arcs_stanford(input_string):
 	2. list of IndexedWord objects
 	'''
 	words = set()
-	deparcs = []
+	dep_arcs = []
 
 	for line in input_string.splitlines():
-		elems = [e for e in re.split(stanford_re, line) if e not in filter_out]
-		new_arc = []
-		for word in elems[1:]:
-			word_elems = word.split('-')
-			new_word = IndexedWord(word_elems[0],int(word_elems[1]))
-			new_arc.append(new_word)
-			words.add(new_word)
-		print(elems[0])
-		is_root = True if elems[0]=='root' else False
-		deparcs.append(DependencyArc(new_arc[0],new_arc[1],elems[0],is_root))
+		arc_parts = []
+		elems = list(filter(None, re.split('[\(\)]+',line,maxsplit=2)))
+		arc_parts.append(elems[0])
+
+		arc_words = [w.strip() for w in filter(None, re.split(',',elems[1],maxsplit=1))]
+		for w in arc_words:
+			arc_parts += w.rsplit('-',1)
+		
+		new_arc = _generate_arcs(arc_parts)
+
+		words.add(IndexedWord(arc_parts[1],int(arc_parts[2])))
+		words.add(IndexedWord(arc_parts[3],int(arc_parts[4])))
+
+		dep_arcs.append(new_arc)
 
 	words_sorted = sorted(list(words), key=lambda x: x.index)
-	return words_sorted,deparcs
+	return words_sorted,dep_arcs
 
 
 def generate_dep_arcs_candc(input_string):
-	pass
+	'''
+	Take list of grammatical relations and return
+	1. list of DependencyArc objects
+	2. list of IndexedWord objects
+	'''
 
+	words = set()
+	dep_arcs = []
+
+	for line in input_string.splitlines():
+		arc_parts = []
+		elems = list(filter(None, re.split(' ',line[1:len(line)-1],maxsplit=5)))
+
+		# Handle special cases
+		if elems[0]=='ncsubj':
+			elems[0] = elems[0]+':'+elems[-1] if elems[-1]!='_' else elems[0]
+			del elems[-1]
+		elif elems[0]=='xcomp':
+			if elems[1]!='_':
+				parts = elems[1].rsplit('_',1)
+				words.add(IndexedWord(parts[0],int(parts[1])))
+				elems[0]='xcomp:{}'.format(parts[0])
+			del elems[1]
+		if elems[0]=='cmod':
+			if elems[1]!='_':
+				parts = elems[1].rsplit('_',1)
+				words.add(IndexedWord(parts[0],int(parts[1])))
+				elems[0]='cmod:{}'.format(parts[0])
+				del[elems[1]]
+		elif len(elems)==4:
+			elems[0] = elems[0]+':'+elems[1] if elems[1]!='_' else elems[0]
+			del elems[1]
+
+		arc_parts.append(elems[0])
+
+		for w in elems[1:]:
+			parts = [part for part in w.rsplit('_',1) if part is not '']
+			arc_parts+=parts
+
+		new_arc = _generate_arcs(arc_parts)
+		words.add(IndexedWord(arc_parts[1],int(arc_parts[2])))
+		words.add(IndexedWord(arc_parts[3],int(arc_parts[4])))
+
+		dep_arcs.append(new_arc)
+
+	words_sorted = sorted(list(words), key=lambda x: x.index)
+	return words_sorted,dep_arcs
+	
 def handle_root_arc(sent,dep_arcs):
 	root_str = ''
 	root_arcs = [a for a in dep_arcs if a.is_root==True]
@@ -92,7 +159,7 @@ def make_dep_arcs(dep_arcs):
 		start = arc.start_word.index
 		end = arc.end_word.index
 		label = arc.label
-		current_arc_str = u"\\depedge{%d}{%d}{%s}\n"%(start,end,label)
+		current_arc_str = u"\\depedge{%s}{%s}{%s}\n"%(start,end,label)
 		arc_str += current_arc_str
 
 	return arc_str
@@ -116,86 +183,51 @@ def write_tex_doc(sent,dep_arcs,full_dir,filename):
 	pass
 
 
-
-# def generate_tikz_stanford(input_string):
-# 	dmp_file = open('./dmp.tex','w')
-# 	dmp_file.writelines(preamble)
-
-# 	words = set()
-# 	deparcs = []
-
-# 	for line in input_string.splitlines():
-# 		elems = [e for e in re.split(stanford_re, line) if e not in filter_out]
-# 		new_arc = []
-# 		for word in elems[1:]:
-# 			word_elems = word.split('-')
-# 			new_word = IndexedWord(word_elems[0],int(word_elems[1]))
-# 			new_arc.append(new_word)
-# 			words.add(new_word)
-
-# 		deparcs.append(DependencyArc(naew_arc[0],new_arc[1],elems[0]))
-# 		#words_formatted = ['({}) {}'.format(w.index,w.word) for w in words]
-# 		#g.edge(words_formatted[0],words_formatted[1],label=elems[0])
-
-# 	print(deparcs)
-# 	print(words)
-# 	words_sorted = sorted(list(words), key=lambda x: x.index)
-# 	ordered_sentence = [w.word for w in words_sorted]
-# 	sent_str = ' \& '.join(ordered_sentence[1:]) + " \\\\\n"
-# 	dmp_file.writelines('\\begin{deptext}\n')
-# 	dmp_file.writelines(sent_str)
-# 	dmp_file.writelines('\\end{deptext}\n')
-	
-# 	for arc in deparcs:
-# 		print(arc)
-# 		start = arc.start_word.index
-# 		print(start)
-# 		end = arc.end_word.index
-# 		print(end)
-# 		label = arc.label
-# 		print(label)
-# 		arc_str = u"\\depedge{%d}{%d}{%s}\n"%(start,end,label)
-# 		print(arc_str)
-# 		dmp_file.writelines(arc_str)
-
-# 	dmp_file.writelines(postamble)
-# 	dmp_file.close()
-
-
-# 	return ordered_sentence
-
 if __name__ == '__main__':
 
-	stanford_deps = """det(car-3, The-1)
-amod(car-3, old-2)
-nsubj(broke-4, car-3)
-root(ROOT-0, broke-4)
-compound:prt(broke-4, down-5)
-case(park-9, in-6)
-det(park-9, the-7)
-compound(park-9, car-8)
-nmod:in(broke-4, park-9)
-"""
+    parses = [
+    {'dir':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/pcfg_stanford_parse_output.txt',
+    'output':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/stanford-pcfg-tex',
+    'type':'stanford'},
+    {'dir':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/nn_stanford_parse_output.txt',
+    'output':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/stanford-nn-tex',
+     'type':'stanford'},
+	{'dir':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/ccg_candc_parse_output_clean.txt',
+	 'output':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/candc-raw-tex',
+	 'type':'candc'},
+	{'dir':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/ccg_candc_parse_punc_output_clean.txt',
+	 'output':'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/output/candc-punc-tex',
+	 'type':'candc'}
+    ]
 
-	candc_deps = '''(ncmod _ car_2 old_1)
-(det car_2 The_0)
-(xcomp _ broke_3 down_4)
-(ncmod _ park._8 car_7)
-(det park._8 the_6)
-(dobj in_5 park._8)
-(ncmod _ broke_3 in_5)
-(ncsubj broke_3 car_2 _)
-'''
+    for parse in parses:
+    	with open(parse['dir'],'r') as fh:
+    		text = fh.read()
+    	parse_list = text.split('\n\n')
+    	print(parse['type'])
+    	for p in parse_list:
+    		print('Running afterparsey for {} of {}'.format(parse_list.index(p),len(parse_list)))
+    		if parse['type']=='stanford':
+    			sent, dep_arcs = generate_dep_arcs_stanford(p)
+    		elif parse['type']=='candc':
+    			sent, dep_arcs = generate_dep_arcs_candc(p)
+    		print(' '.join(w.word for w in sent))
+    		write_tex_fig(sent,dep_arcs,parse['output'],'parse{}.tex'.format(parse_list.index(p)),'column sep=0.5cm')
 
-	#a = stanford_generator(stanford_deps)
-	#a.view()
+	#print(sent1)
+	#print()
+	#print(dep_arcs1)
 
-	#b = candc_generator(candc_deps)
-	#b.view()
-	#print(generate_tikz_stanford(stanford_deps))
+    # with open(stanford_parse1,'r') as fh:
+    # 	text = fh.read()
 
-	sent1, dep_arcs1 = generate_dep_arcs_stanford(stanford_deps)
+    # parses = text.split('\n\n')
+    # for l in parses:
+    # 	print('next parse')
+    # 	print('>>>>>>>>>>>>>>>>>>>>>>>>>')
+    # 	print(l)
+    # 	print('<<<<<<<<<<<<<<<<<<<<<<<<<')
 
-	print(' '.join(w.word for w in sent1))
-	print([da for da in dep_arcs1 if da.is_root==True])
-	write_tex_fig(sent1,dep_arcs1,'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/viz/test','test.tex','column sep=0.5cm')
+	#sent1, dep_arcs1 = generate_dep_arcs_stanford(stanford_deps)
+	#print([da for da in dep_arcs1 if da.is_root==True])
+	#write_tex_fig(sent1,dep_arcs1,'/Users/tom/Desktop/Cambridge/L95-SyntaxParsing/parsers/viz/test','test.tex','column sep=0.5cm')
